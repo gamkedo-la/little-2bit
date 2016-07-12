2783523d94a1b59565e98cc287e8b4e9d9d4a87d
Caspar Dunant <spadxiii@gmail.com>
2016-07-12 19:43:41 +0200

    Fix left/right arrow key bindings

--- js/input.js
+++ js/input.js
@@ -42,12 +42,12 @@ function keyReleased(event) {
 }
 
 function keySet(event, setTo) {
-  if (event.keyCode == KEY_A || event.keyCode == KEY_RIGHT_ARROW) {
+  if (event.keyCode == KEY_A || event.keyCode == KEY_LEFT_ARROW) {
     Ship.keyHeld_W = setTo;
     UI.keyHeld_W = setTo;
     event.preventDefault();
   }
-  if (event.keyCode == KEY_D || event.keyCode == KEY_LEFT_ARROW) {
+  if (event.keyCode == KEY_D || event.keyCode == KEY_RIGHT_ARROW) {
     UI.keyHeld_E = setTo;
     Ship.keyHeld_E = setTo;
     event.preventDefault();
